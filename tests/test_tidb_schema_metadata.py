09847c5632d8062347c32cb6032a23d8dfc02485
DevingGrosko <79125705+DevingGrosko@users.noreply.github.com>
2026-09-20 20:28:54 -0400

    test(staging): validate auto-increment bases in TiDB table-options metadata

--- tests/test_tidb_schema_metadata.py
+++ tests/test_tidb_schema_metadata.py
@@ -1,5 +1,5 @@
 import unittest
-from tools.tidb_schema_rehearsal import normal_index
+from tools.tidb_schema_rehearsal import normal_index, declared_auto_increment, RehearsalError
 
 
 class IndexMetadataTests(unittest.TestCase):
@@ -13,3 +13,20 @@ class IndexMetadataTests(unittest.TestCase):
     def test_invalid_flag_is_rejected(self):
         with self.assertRaises(ValueError):
             normal_index(('k', 'unknown', 1, 'x'))
+
+
+class AutoIncrementMetadataTests(unittest.TestCase):
+    def test_reads_tidb_table_option_not_column_attribute(self):
+        ddl = "CREATE TABLE `event` (\n  `id` int NOT NULL AUTO_INCREMENT\n) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=313"
+        self.assertEqual(declared_auto_increment(ddl), 313)
+
+    def test_reordered_options_preserve_the_base(self):
+        self.assertEqual(declared_auto_increment("CREATE TABLE `x` (\n `id` int\n) ENGINE=InnoDB AUTO_INCREMENT=5759850 DEFAULT CHARSET=utf8"), 5759850)
+
+    def test_missing_table_option_is_rejected(self):
+        with self.assertRaises(RehearsalError):
+            declared_auto_increment("CREATE TABLE `x` (\n `id` int AUTO_INCREMENT\n) ENGINE=InnoDB DEFAULT CHARSET=utf8")
+
+    def test_does_not_read_an_auto_increment_number_from_a_column_default(self):
+        with self.assertRaises(RehearsalError):
+            declared_auto_increment("CREATE TABLE `x` (\n `note` varchar(30) DEFAULT 'AUTO_INCREMENT=999'\n) ENGINE=InnoDB")
